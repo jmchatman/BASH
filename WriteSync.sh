bc9032cccccc6e7d63000857926579dddd5fd46f
James Chatman <admin@marionchatman.com>
2019-01-03 08:00:58 -0500

    2019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 22019-01-03 Update 2

---
+++ WriteSync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rsync -avt ~/Documents/RETURN\ NOVELS/ /Volumes/XBOOT/Files/RETURN\ NOVELS/
+
+rsync -avt ~/Documents/RETURN\ PUBLISHING/ /Volumes/XBOOT/Files/RETURN\ PUBLISHING/
+
+exit
